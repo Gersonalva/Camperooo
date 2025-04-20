03a12b8f43c23292f8c7f80cc7fda2e166411e3d
Gersonalva <alvarasgerson457@gmail.com>
2025-04-20 13:27:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ let boton_cerrar = document.getElementById('X');
 
 function abrir_cerrar_menu(){
     menu_desplegable.classList.toggle('abrir_menu');
-    boton_cerrar.classList.toggle('colocar_X');
+    boton_cerrar.classList.toggle('colocar_x');
 }
